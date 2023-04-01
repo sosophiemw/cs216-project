08d05a356283f5ae03f2df6d2bf331ce5b6f10bc
Sonya PatelE <sp465@duke.edu>
2023-04-01 14:19:13 -0400

    fixed key leak

--- spotify_data.py
+++ spotify_data.py
@@ -10,15 +10,6 @@ sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
 
 global_top_50_songs = 'https://open.spotify.com/playlist/37i9dQZEVXbNG2KDcFcKOF?si=77d8f5cd51cd478d'
 
-# curl -X POST "https://accounts.spotify.com/api/token" \
-#      -H "Content-Type: application/x-www-form-urlencoded" \
-#      -d "grant_type=client_credentials&client_id=45c623d291f7413894b994f01d0f7bef&client_secret=a9c8c74eb8764953b36d70e8ea330c84"
-
-# {"access_token":"BQBjg1Ch-dv4VMxFgQSeH4Z3P11lrM3e4yu-Lc8-UXsBmKHKHvRAnhL-oheabDd_YOlKxawTHHMB-jQKZ8PLOvCj84kF3CR42vOLa0iI4SC2TVbDX9Rb","token_type":"Bearer","expires_in":3600}%     
-
-# curl "https://api.spotify.com/v1/artists/4Z8W4fKeB5YxbusRsdQVPb" \
-#      -H "Authorization: Bearer  BQBjg1Ch-dv4VMxFgQSeH4Z3P11lrM3e4yu-Lc8-UXsBmKHKHvRAnhL-oheabDd_YOlKxawTHHMB-jQKZ8PLOvCj84kF3CR42vOLa0iI4SC2TVbDX9Rb"
-    
 
 playlist_link = "https://open.spotify.com/playlist/37i9dQZEVXbNG2KDcFcKOF?si=1333723a6eff4b7f"
 playlist_URI = playlist_link.split("/")[-1].split("?")[0]
