1f5441e1b6ca0fa209c192d781c1b23b44e56979
Aleksandra Koroza <ak559@duke.edu>
2023-04-23 12:25:41 -0400

    master dataset created

--- plots.py
+++ plots.py
@@ -10,5 +10,17 @@ features4 = pd.read_csv('features4.csv')
 features5 = pd.read_csv('features5.csv')
 
 df = pd.concat([features2, features3, features4, features5])
-print(df.head())
 df.to_csv('features_all.csv')
+
+first = pd.read_csv('first.csv')
+second = pd.read_csv('second.csv')
+third = pd.read_csv('third.csv')
+fourth = pd.read_csv('fourth.csv')
+fifth = pd.read_csv('fifth.csv')
+sixth = pd.read_csv('sixth.csv')
+
+df2 = pd.concat([first, second, third, fourth])
+df2.to_csv('first-sixth_all.csv')
+
+df_merged = df2.merge(df, how='inner', left_on='track_uri', right_on='uri')
+df_merged.to_csv('merged_data.csv')
